a8da41e260345d92ac6af3eebfa0eb29d2586e66
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2023-12-23 21:43:12 +0530

    Checkout and product page

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import {
   Link,
 } from "react-router-dom";
 import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 const router = createBrowserRouter([
   {
@@ -31,6 +33,14 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage />,
+  },
+  {
+    path: "/checkout",
+    element: <CheckoutPage />,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetailPage />,
   },
 ]);
 
