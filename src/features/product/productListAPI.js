720994dfbd1103e50f64f15ac81398b4abd378a4
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2023-12-24 18:17:29 +0530

    updated filters and products

--- src/features/product/productListAPI.js
+++ src/features/product/productListAPI.js
@@ -9,13 +9,22 @@ export function fetchAllProducts(amount = 1) {
 }
 
 
-export function fetchProductsByFilters(filter) {
+export function fetchProductsByFilters(filter, sort) {
 
   // filter object
   let queryString ='';
   for (let key in filter){
-    queryString+=`${key}=${filter[key]}&`
+    const categoryValue = filter[key];
+    if(categoryValue.length){
+      const lastCategoryvalue = categoryValue[categoryValue.length-1]
+      queryString+=`${key}=${lastCategoryvalue}&`
+    }
   }
+
+  for(let key in sort){
+    queryString+=`${key}=${sort[key]}&`
+  }
+
   return new Promise( async (resolve) => {
       const response = await fetch('http://localhost:8080/products?'+queryString);
       const data = await response.json();
